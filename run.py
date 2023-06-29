878ecdc47271f6401665b90e39b004c643caa012
bek <bekrye@gmail.com>
2023-06-29 17:57:57 +0200

    Add POST method in contact form

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template  #Flask is a class
+from flask import Flask, render_template, request  #Flask is a class
 
 # craete an instance of this class
 app = Flask(__name__)
@@ -26,8 +26,11 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title='Contact')
 
 @app.route("/careers")
