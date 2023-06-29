03c391036ab8e7a685b740479d0cd9c923e2bb14
bek <bekrye@gmail.com>
2023-06-29 20:39:41 +0200

    deploy to heroku

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import os
 import json
-from flask import Flask, render_template, request  #Flask is a class
+from flask import Flask, render_template, request, flash  #Flask is a class
+if os.path.exists("env.py"):
+    import env
+
 
 # craete an instance of this class
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -29,8 +33,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title='Contact')
 
 @app.route("/careers")
